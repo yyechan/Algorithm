db9ff03b4db2284cfc946d6fe7f25417f3ce2f07
yyechan <45142084+yyechan@users.noreply.github.com>
2020-09-18 01:28:45 +0900

    update

--- JONGMAN/200906_PICNIC.cpp
+++ JONGMAN/200906_PICNIC.cpp
@@ -12,30 +12,23 @@ int answer;
 void dfs(vector<vector<int>> &vec , int dep ,int C) {
 
 	if (dep == N / 2) {
-
 		answer++;
 		return;
-
 	}
-
 
 	for (int i = C; i < N; i++) {
 
-		if(isMatched[i] == false)
+		if(isMatched[i] == false) {
 			for (int j = 0; j < vec[i].size(); j++) {
 
 				if (isMatched[vec[i][j]] == false) {
-
 					isMatched[i] = isMatched[vec[i][j]] = true;
 					dfs(vec, dep + 1,i+1);
 					isMatched[i] = isMatched[vec[i][j]] = false;
 				}
-
 			}
-
+		}
 	}
-
-
 }
 
 
@@ -47,33 +40,20 @@ void solve() {
 	cin >> N >> M;
 
 	vector<vector<int>> vec(N);
-
-
-
 	for (int i = 0; i < M; i++) {
-
 		int n1, n2;
-
 		cin >> n1 >> n2;
-
 		vec[min(n1, n2)].push_back(max(n1, n2));
-
 	}
-
-
-	dfs(vec, 0,0);
-
-
+	
+	dfs(vec, 0,0);	
 	cout << answer << endl;
-
 }
 
 
 int main(int argc, char** argv) {
-
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-
 	int T;
 
 	cin >> T;
